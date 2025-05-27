bb691b17f1de373f2c4516796139a787749a1fcf
József Vadkerti <jovadker@microsoft.com>
2025-05-27 12:38:30 +0200

    added a storage account to the sandbox template

--- Environments/Terraform-Sandbox/main.tf
+++ Environments/Terraform-Sandbox/main.tf
@@ -15,3 +15,19 @@ resource "azurerm_resource_group" "example" {
   name     = "ade-terraform-resource-group"
   location = "East US"
 }
+
+resource "azurerm_storage_account" "example" {
+  name                     = "adeterraformsa${random_integer.suffix.result}"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "random_integer" "suffix" {
+  min = 10000
+  max = 99999
+  keepers = {
+    resource_group = azurerm_resource_group.example.name
+  }
+}
